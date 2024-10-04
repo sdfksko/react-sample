0a8774167043fea46b567860eb16fc8745052ece
noua026 <noua026@gmail.com>
2024-10-04 09:09:49 +0900

    삭제

--- spring-react/src/main/java/com/example/spring_react/config/oauth/select/OAuth2UserInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring_react.config.oauth.select;
-
-public interface OAuth2UserInfo {
-    String getProvider();
-    String getProviderId();
-    String getEmail();
-    String getName();
-}
--- spring-react/src/main/java/com/example/spring_react/restcontroller/LoginRestController.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_react.restcontroller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginRestController {
-
-
-}

8be7832df25b934754dfa57068e0ede28f6ae5a9
sdfksko <noua026@gmail.com>
2024-10-20 14:29:50 +0900

    수정본

--- spring-react/src/main/frontend/src/pages/adminPages/AdminMemberPage.js
+++
@@ -1,9 +0,0 @@
-import AdminMember from '../../components/admin/AdminMember.js';
-
-function AdminMemberPage() {
-    return(
-        <AdminMember />
-    )
-}
-
-export default AdminMemberPage;
--- spring-react/src/main/frontend/src/pages/adminPages/AdminPage.js
+++
@@ -1,9 +0,0 @@
-import Admin from '../../components/admin/Admin.js';
-
-function AdminPage() {
-    return(
-        <Admin />
-    )
-}
-
-export default AdminPage;
